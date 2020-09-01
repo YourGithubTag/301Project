f70b684ba35bdc9b1859e7220f1b0a7c4615aa5b
Cecil Symes <57969872+CCP115@users.noreply.github.com>
2020-09-01 16:06:18 +1200

    Poke around
    Spent time trying to understand how the robot moves and works

--- FungYangCs301PacmanUseLib/fungYangCs301SimMain.cpp
+++ FungYangCs301PacmanUseLib/fungYangCs301SimMain.cpp
@@ -55,85 +55,17 @@ void setVirtualCarSpeed(float linearSpeed, float angularSpeed)
 float virtualCarLinearSpeed_seed;
 float virtualCarAngularSpeed_seed;
 
-int virtualCarInit()
-{
-	//sensorPopulationAlgorithmID = PLACE_SENSORS_AUTO_SEP;
-	sensorPopulationAlgorithmID = PLACE_SENSORS_SEP_USER_DEFINED;
-	num_sensors = 7;
-	sensorSeparation = 0.08;
+/******************** USER DEFINED FUNCTIONS ********************/
 
-	virtualCarLinearSpeed_seed = 0.6;
-	virtualCarAngularSpeed_seed = 40;
-	currentCarPosCoord_X = 6;
-	currentCarPosCoord_Y = -3;
-	currentCarAngle = 90;
-
-	return 1;
+void turnLeftAtSpeed(int speedInput) {
+	speedInput = -speedInput;
+	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
 }
 
-int virtualCarUpdate()
-{
-	//{----------------------------------
-	//process sensor state information
-	float halfTiltRange = (num_sensors - 1.0) / 2.0;
-	float tiltSum = 0.0;
-	float blackSensorCount = 0.0;
-	for (int i = 0; i < num_sensors; i++)
-	{
-		if (virtualCarSensorStates[i] == 0)
-		{
-			float tilt = (float)i - halfTiltRange;
-			tiltSum += tilt;
-			blackSensorCount += 1.0;
-		}
-	}
-	//}------------------------------------
-
-	//{------------------------------------
-	//updat linear and rotational speed based on sensor information
-	if (blackSensorCount > 0.0)
-		setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * tiltSum);
-	//setVirtualCarSpeed(0.60, 40.0*tiltSum);
-	else
-		setVirtualCarSpeed(0.0, virtualCarAngularSpeed_seed);
-	//setVirtualCarSpeed(0.0, 40.0);
-//}---------------------------------------
-
-//below is optional. just to provid some status report and function test result .
-//You can try to use "printf()" to reimplemet this "cout" c++ section in a c style instead.
-//{--------------------------------------------------------------	
-	if (myTimer.getTimer() > 0.5)
-	{
-		myTimer.resetTimer();
-
-		cout << "=====================================" << endl;
-		cout << "current car X, Y, theta = " << currentCarPosCoord_X << " , " << currentCarPosCoord_Y << " , " << currentCarAngle << endl;
-		cout << "current Cell X, Y = " << coordToCellX(currentCarPosCoord_X) << " , " << coordToCellY(currentCarPosCoord_Y) << endl;
-		cout << "-----------------------------------------" << endl;
-		cout << " ghost list info:" << endl;
-		for (int i = 0; i < ghostInfoPackList.size(); i++)
-		{
-			cout << "g[" << i << "]: (" << ghostInfoPackList[i].coord_x << ", " << ghostInfoPackList[i].coord_y << "); [s=" <<
-				ghostInfoPackList[i].speed << "; [d=" << ghostInfoPackList[i].direction << "]; [T=" << ghostInfoPackList[i].ghostType << "]" << endl;
-		}
-		cout << "-----------------------------------------" << endl;
-		int randNumber = rand_nextInt(10);
-		cout << " a rand number between 0 ~ 10 = " << randNumber << endl;
-		randNumber = rand_nextInt(10, 20);
-		cout << " a rand number between 10 ~ 20 = " << randNumber << endl;
-		cout << "-----------------------------------------" << endl;
-		cout << "map[0][9] = " << map[0][9] << endl;
-		cout << "food_list[5][0] = " << food_list[5][0] << endl;
-	}
-	//}---------------------------------------------------------------
-
-	return 1;
+void turnRightAtSpeed(int speedInput) {
+	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
 }
-//}=============================================================
 
-
-
-// NEW CODE HERE
 void turnLeft() {
 	bool pathDetected = 0;
 	for (int j = 0; j < num_sensors; j++) {
@@ -184,15 +116,6 @@ void turnRight() {
 	}
 }
 
-void turnLeftAtSpeed(int speedInput) {
-	speedInput = -speedInput;
-	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
-}
-
-void turnRightAtSpeed(int speedInput) {
-	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
-}
-
 void turnLeftOnSpot(int speedInput) {
 	speedInput = -speedInput;
 	setVirtualCarSpeed(0, virtualCarAngularSpeed_seed * speedInput);
@@ -202,6 +125,107 @@ void turnRightOnSpot(int speedInput) {
 	setVirtualCarSpeed(0, virtualCarAngularSpeed_seed * speedInput);
 }
 
+
+
+/******************** CORE FUNCTIONS ********************/
+
+int virtualCarInit()
+{
+	//sensorPopulationAlgorithmID = PLACE_SENSORS_AUTO_SEP;
+	sensorPopulationAlgorithmID = PLACE_SENSORS_SEP_USER_DEFINED;
+	num_sensors = 7;
+	sensorSeparation = 0.08;
+
+	virtualCarLinearSpeed_seed = 0.5;
+	virtualCarAngularSpeed_seed = 40;
+	currentCarPosCoord_X = 6;
+	currentCarPosCoord_Y = -3;
+	currentCarAngle = 90;
+
+	return 1;
+}
+
+int virtualCarUpdate()
+{
+	float linspeed = virtualCarLinearSpeed_seed;
+	float angspeed = virtualCarAngularSpeed_seed;
+	// Test
+
+
+	setVirtualCarSpeed(0.5, 40);
+
+	/*TODO
+	* 1. Read sensor
+	* - Can just use YungFang's code
+	* - Move to another function maybe
+	* - Determine which are active, e.g. -ve is left, +ve right
+	* - Value determines srength, e.g. -1 is slight left turn, 3 is strong right turn
+	* 2. Detect intersection
+	* - E.g. are the far left/far right sensors active? Is it a T or a pure turn? No 4 intersections luckily
+	* - Can we move the sensors around?
+	* 3. What do we do at a dead end?
+	* 4. What do we do off track?
+	* 5. When do we call director and algo?
+	*/
+
+	/*
+	
+	// Process sensor state information
+	float halfTiltRange = (num_sensors - 1.0) / 2.0;
+	float tiltSum = 0.0;
+	float blackSensorCount = 0.0;
+	for (int i = 0; i < num_sensors; i++)
+	{
+		if (virtualCarSensorStates[i] == 0)
+		{
+			float tilt = (float)i - halfTiltRange;
+			tiltSum += tilt;
+			blackSensorCount += 1.0;
+		}
+	}
+
+	// Update linear and rotational speed based on sensor information
+	if (blackSensorCount > 0.0)
+	{
+		setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * tiltSum);
+	}
+	else
+	{
+		setVirtualCarSpeed(0.0, virtualCarAngularSpeed_seed);
+	}
+
+//below is optional. just to provid some status report and function test result .
+//You can try to use "printf()" to reimplemet this "cout" c++ section in a c style instead.
+//{--------------------------------------------------------------	
+	if (myTimer.getTimer() > 1)
+	{
+		myTimer.resetTimer();
+
+		cout << "=====================================" << endl;
+		cout << "current car X, Y, theta = " << currentCarPosCoord_X << " , " << currentCarPosCoord_Y << " , " << currentCarAngle << endl;
+		cout << "current Cell X, Y = " << coordToCellX(currentCarPosCoord_X) << " , " << coordToCellY(currentCarPosCoord_Y) << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << " ghost list info:" << endl;
+		for (int i = 0; i < ghostInfoPackList.size(); i++)
+		{
+			cout << "g[" << i << "]: (" << ghostInfoPackList[i].coord_x << ", " << ghostInfoPackList[i].coord_y << "); [s=" <<
+				ghostInfoPackList[i].speed << "; [d=" << ghostInfoPackList[i].direction << "]; [T=" << ghostInfoPackList[i].ghostType << "]" << endl;
+		}
+		cout << "-----------------------------------------" << endl;
+		int randNumber = rand_nextInt(10);
+		cout << " a rand number between 0 ~ 10 = " << randNumber << endl;
+		randNumber = rand_nextInt(10, 20);
+		cout << " a rand number between 10 ~ 20 = " << randNumber << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << "map[0][9] = " << map[0][9] << endl;
+		cout << "food_list[5][0] = " << food_list[5][0] << endl;
+	}
+	//}---------------------------------------------------------------
+
+	*/
+
+	return 1;
+}
 
 
 int main(int argc, char** argv)
