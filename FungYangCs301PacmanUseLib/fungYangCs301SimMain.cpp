2f4b2ad58fdc3f9152696bec457ef742c91988ce
Cecil Symes <57969872+CCP115@users.noreply.github.com>
2020-09-26 15:57:38 +1200

    Added some movement functions
    -Added gitignore

--- FungYangCs301PacmanUseLib/fungYangCs301SimMain.cpp
+++ FungYangCs301PacmanUseLib/fungYangCs301SimMain.cpp
@@ -59,14 +59,7 @@ float virtualCarAngularSpeed_seed;
 
 /******************** USER DEFINED FUNCTIONS ********************/
 
-void turnLeftAtSpeed(int speedInput) {
-	speedInput = -speedInput;
-	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
-}
-
-void turnRightAtSpeed(int speedInput) {
-	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
-}
+// Data Functions
 
 void ConvertToIntersectionMap() {
 	for (int i = 0; i < 15; i++) {
@@ -82,7 +75,7 @@ void ConvertToIntersectionMap() {
 					paths++;
 				}
 			}
-			if ((i+1) <= 14) {
+			if ((i + 1) <= 14) {
 				if (map[i + 1][j] == 0) {
 					paths++;
 				}
@@ -100,6 +93,78 @@ void ConvertToIntersectionMap() {
 			}
 		}
 	}
+}
+
+void statusReport()
+{
+	// Prints a status report to the console every tick, taken from the default code provided
+	//below is optional. just to provid some status report and function test result .
+	//You can try to use "printf()" to reimplemet this "cout" c++ section in a c style instead.
+	//{--------------------------------------------------------------	
+	if (myTimer.getTimer() > 1)
+	{
+		myTimer.resetTimer();
+
+		cout << "=====================================" << endl;
+		cout << "current car X, Y, theta = " << currentCarPosCoord_X << " , " << currentCarPosCoord_Y << " , " << currentCarAngle << endl;
+		cout << "current Cell X, Y = " << coordToCellX(currentCarPosCoord_X) << " , " << coordToCellY(currentCarPosCoord_Y) << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << " ghost list info:" << endl;
+		for (int i = 0; i < ghostInfoPackList.size(); i++)
+		{
+			cout << "g[" << i << "]: (" << ghostInfoPackList[i].coord_x << ", " << ghostInfoPackList[i].coord_y << "); [s=" <<
+				ghostInfoPackList[i].speed << "; [d=" << ghostInfoPackList[i].direction << "]; [T=" << ghostInfoPackList[i].ghostType << "]" << endl;
+		}
+		cout << "-----------------------------------------" << endl;
+		int randNumber = rand_nextInt(10);
+		cout << " a rand number between 0 ~ 10 = " << randNumber << endl;
+		randNumber = rand_nextInt(10, 20);
+		cout << " a rand number between 10 ~ 20 = " << randNumber << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << "map[0][9] = " << map[0][9] << endl;
+		cout << "food_list[5][0] = " << food_list[5][0] << endl;
+	}
+	//}---------------------------------------------------------------
+}
+
+
+// Movement Functions
+
+void dumbLineFollow()
+{
+	// Follows the line blindly, taken from the code given to us
+	// Process sensor state information
+	float halfTiltRange = (num_sensors - 1.0) / 2.0;
+	float tiltSum = 0.0;
+	float blackSensorCount = 0.0;
+	for (int i = 0; i < num_sensors; i++)
+	{
+		if (virtualCarSensorStates[i] == 0)
+		{
+			float tilt = (float)i - halfTiltRange;
+			tiltSum += tilt;
+			blackSensorCount += 1.0;
+		}
+	}
+
+	// Update linear and rotational speed based on sensor information
+	if (blackSensorCount > 0.0)
+	{
+		setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * tiltSum);
+	}
+	else
+	{
+		setVirtualCarSpeed(0.0, virtualCarAngularSpeed_seed);
+	}
+}
+
+void turnLeftAtSpeed(int speedInput) {
+	speedInput = -speedInput;
+	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
+}
+
+void turnRightAtSpeed(int speedInput) {
+	setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * speedInput);
 }
 
 void turnLeft() {
@@ -196,10 +261,6 @@ int virtualCarUpdate()
 {
 	float linspeed = virtualCarLinearSpeed_seed;
 	float angspeed = virtualCarAngularSpeed_seed;
-	// Test
-
-
-	setVirtualCarSpeed(0.5, 40);
 
 	/*TODO
 	* 1. Read sensor
@@ -215,61 +276,9 @@ int virtualCarUpdate()
 	* 5. When do we call director and algo?
 	*/
 
-	/*
-	
-	// Process sensor state information
-	float halfTiltRange = (num_sensors - 1.0) / 2.0;
-	float tiltSum = 0.0;
-	float blackSensorCount = 0.0;
-	for (int i = 0; i < num_sensors; i++)
-	{
-		if (virtualCarSensorStates[i] == 0)
-		{
-			float tilt = (float)i - halfTiltRange;
-			tiltSum += tilt;
-			blackSensorCount += 1.0;
-		}
-	}
-
-	// Update linear and rotational speed based on sensor information
-	if (blackSensorCount > 0.0)
-	{
-		setVirtualCarSpeed(virtualCarLinearSpeed_seed, virtualCarAngularSpeed_seed * tiltSum);
-	}
-	else
-	{
-		setVirtualCarSpeed(0.0, virtualCarAngularSpeed_seed);
-	}
-
-//below is optional. just to provid some status report and function test result .
-//You can try to use "printf()" to reimplemet this "cout" c++ section in a c style instead.
-//{--------------------------------------------------------------	
-	if (myTimer.getTimer() > 1)
-	{
-		myTimer.resetTimer();
-
-		cout << "=====================================" << endl;
-		cout << "current car X, Y, theta = " << currentCarPosCoord_X << " , " << currentCarPosCoord_Y << " , " << currentCarAngle << endl;
-		cout << "current Cell X, Y = " << coordToCellX(currentCarPosCoord_X) << " , " << coordToCellY(currentCarPosCoord_Y) << endl;
-		cout << "-----------------------------------------" << endl;
-		cout << " ghost list info:" << endl;
-		for (int i = 0; i < ghostInfoPackList.size(); i++)
-		{
-			cout << "g[" << i << "]: (" << ghostInfoPackList[i].coord_x << ", " << ghostInfoPackList[i].coord_y << "); [s=" <<
-				ghostInfoPackList[i].speed << "; [d=" << ghostInfoPackList[i].direction << "]; [T=" << ghostInfoPackList[i].ghostType << "]" << endl;
-		}
-		cout << "-----------------------------------------" << endl;
-		int randNumber = rand_nextInt(10);
-		cout << " a rand number between 0 ~ 10 = " << randNumber << endl;
-		randNumber = rand_nextInt(10, 20);
-		cout << " a rand number between 10 ~ 20 = " << randNumber << endl;
-		cout << "-----------------------------------------" << endl;
-		cout << "map[0][9] = " << map[0][9] << endl;
-		cout << "food_list[5][0] = " << food_list[5][0] << endl;
-	}
-	//}---------------------------------------------------------------
-
-	*/
+	dumbLineFollow();
+	statusReport();
+	cin << << endl;
 
 	return 1;
 }
